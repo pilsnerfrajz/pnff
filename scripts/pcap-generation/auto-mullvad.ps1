1796c15c56caaded9210b8288375d3284611f52c
William Hedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2026-08-01 16:39:10 +0200

    Improve PCAP Generation Script (#5)
    * Remove unused param, and update for-loop
    
    * Update param name for usability
    
    * Fix forgotten name change

--- scripts/pcap-generation/auto-mullvad.ps1
+++ scripts/pcap-generation/auto-mullvad.ps1
@@ -1,8 +1,7 @@
 #Requires -RunAsAdministrator
 
 param (
-    [string]$MullvadAccount,
-    [string]$Interface,
+    [string]$NetworkInterface,
     [string]$OutputFile
 )
 
@@ -15,7 +14,6 @@ function Invoke-LocalMullvadCapture {
 
     Start-Service -Name "MullvadVPN" -ErrorAction SilentlyContinue
     Start-Sleep -Seconds 2
-
     mullvad relay set location se > $null
     mullvad anti-censorship set mode $Mode > $null
 
@@ -23,19 +21,18 @@ function Invoke-LocalMullvadCapture {
         $Mode = "vanilla"
     }
 
-    for ($i = 1; $i -le 50; $i++) {
-        Start-Sleep -Seconds 3 
+    for ($i = 1; $i -le 473; $i++) {
+        Start-Sleep -Seconds 3
 
         $CurrentFile = "${OutputFile}_${Mode}_${i}.pcap"
         echo "Starting capture iteration $i. Output file: $CurrentFile"
 
         # Start tshark in the background and save the process to a variable
-        $tsharkArgs = "-i $Interface -w $CurrentFile"
+        $tsharkArgs = "-i $NetworkInterface -w $CurrentFile"
         $captureProcess = Start-Process -FilePath "C:\Program Files\Wireshark\tshark.exe" -ArgumentList $tsharkArgs -PassThru -WindowStyle Hidden
 
         Start-Service -Name "MullvadVPN" -ErrorAction SilentlyContinue
         Start-Sleep -Seconds 2
-
         mullvad connect -w > $null
 
         echo "Mullvad status: $(mullvad status)"
@@ -51,20 +48,16 @@ function Invoke-LocalMullvadCapture {
         curl.exe -s "https://svtplay.se" > $null
         curl.exe -s "https://svtplay.se/program" > $null
         curl.exe -s "https://svtplay.se/program/historia" > $null
-        
+
         # Stop the local capture process
         $captureProcess | Stop-Process -Force
 
         mullvad disconnect -w > $null
         mullvad status
-
-        Stop-Service -Name "MullvadVPN" -Force        
+        Stop-Service -Name "MullvadVPN" -Force
     }
 }
 
-# Vanilla
 Invoke-LocalMullvadCapture -Mode "off"
-
 Invoke-LocalMullvadCapture -Mode "lwo"
-
 Invoke-LocalMullvadCapture -Mode "shadowsocks"
